6d0e2a3f01327f80f2e5b9e3129cca467724c069
Nazdum <camilo.mendez@uao.edu.co>
2022-03-23 12:55:13 -0500

    eliminados para refactorizacion

--- src/routes/task.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-const { createTask, deleteTask } = require('../controllers/task.controllers');
-
-router.post("/", createTask);
-router.delete("/", deleteTask);
-
-module.exports = router
