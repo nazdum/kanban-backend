7b776768b4f81050a880cfea00b5144ad670b208
Nazdum <camilo.mendez@uao.edu.co>
2022-03-23 12:44:23 -0500

    esquema del usuario(describe como es un usuario y como se representa en la BD

---
+++ src/api/entities/user/user.model.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: { type: String, required: true },
+    passwordHash: { type: String, required: true },
+    tasks: []
+})
+
+const User = model("user", UserSchema)
+
+module.exports = User
