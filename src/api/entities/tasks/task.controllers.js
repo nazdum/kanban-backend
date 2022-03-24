2bf283e309360b28c44c1628a05109d84dfd00f5
Nazdum <camilo.mendez@uao.edu.co>
2022-03-23 19:46:40 -0500

    ahora se pueden crear tasks y estas se relacionan con el modelo de users en la base de datos

--- src/api/entities/tasks/task.controllers.js
+++ src/api/entities/tasks/task.controllers.js
@@ -1,6 +1,24 @@
+const Task = require('./task.model');
+const User = require('../user/user.model');
 
-const createTask = (request, response) => {
-    response.json({ "msg": "createTask" });
+const createTask = async (request, response) => {
+
+    const { title, description, authorID } = request.body;
+
+    const user = await User.findById(authorID)
+    const task = new Task({ title, description, state: "to do", author: authorID });
+
+    try {
+        const savedTask = await task.save()
+        user.tasks.push(savedTask._id)
+        await user.save();
+        response.status(201).json({ message: "task saved successfully", savedTask })
+
+    } catch (error) {
+        response.status(500).json(error)
+    }
+
+
 }
 
 const deleteTask = (request, response) => {
