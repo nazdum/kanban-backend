2b121450c99ec34b76a56a727145936a321b15e4
Nazdum <camilo.mendez@uao.edu.co>
2022-03-23 19:44:44 -0500

    se agrego la referencia al modelo de tasks

--- src/api/entities/user/user.model.js
+++ src/api/entities/user/user.model.js
@@ -3,7 +3,10 @@ const { Schema, model } = require('mongoose');
 const UserSchema = new Schema({
     username: { type: String, required: true },
     passwordHash: { type: String, required: true },
-    tasks: []
+    tasks: [{
+        type: Schema.Types.ObjectId,
+        ref: 'task',
+    }]
 })
 
 const User = model("user", UserSchema)
