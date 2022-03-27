844e5cb8d1235c5cdfd03554fa9a9e5ace86fd38
Nazdum <camilo.mendez@uao.edu.co>
2022-03-27 18:55:56 -0500

    modulo que centraliza la distribución de los middlewares de las tasks

---
+++ src/api/entities/tasks/middlewares/index.js
@@ -0,0 +1,9 @@
+const taskValidator = require('./taskValidator')
+const noteIDValidator = require('./noteIDValidator')
+const exists = require('./exists')
+
+module.exports = {
+    taskValidator,
+    noteIDValidator,
+    exists
+}
