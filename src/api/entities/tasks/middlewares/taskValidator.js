2edb493d914e21bfcdeabb062655b9be1da59074
Nazdum <camilo.mendez@uao.edu.co>
2022-03-27 18:51:41 -0500

    midlleware que se encarga de revisar los parametros de una nueva task

--- src/api/entities/tasks/middlewares/taskValidator.js
+++ src/api/entities/tasks/middlewares/taskValidator.js
@@ -1,18 +1,15 @@
+const regexID = require('../../common/idRegex')
+
 const taskValidator = (request, response, next) => {
 
-    const { title, state, authorID } = request.body;
+    const { title, state } = request.body;
 
-    if (!(title  && state && authorID)) {
-        return response.status(400).json({ error: "missing parameters (title, state or authorID)" });
+    if (!(title && state)) {
+        return response.status(400).json({ error: "missing parameters (title or state)" });
     }
 
     if (title.length < 3) {
         return response.status(400).json({ error: "title must have at least 4 characters" })
-    }
-
-    const regex = /^[a-z0-9]{24}$/
-    if (!regex.test(authorID)) {
-        return response.status(400).json({ error: "invalid authorID format" });
     }
 
     const estados = ["to do", "in progress", "completed"]
