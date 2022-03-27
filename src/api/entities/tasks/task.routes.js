55d533084872ea7b7322199f2f11142d026d1cf7
Nazdum <camilo.mendez@uao.edu.co>
2022-03-27 18:53:22 -0500

    refactorizado para cargra todos los middlewares desde un solo punto

--- src/api/entities/tasks/task.routes.js
+++ src/api/entities/tasks/task.routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
-const { createTask, deleteTask } = require('./task.controllers');
-const taskValidator = require('./middlewares/taskValidator');
+const { createTask, deleteTask, updateTask } = require('./task.controllers');
+const { taskValidator, exists, noteIDValidator } = require('./middlewares/');
 
 router.post("/", taskValidator, createTask);
-router.delete("/", deleteTask);
+router.delete("/:noteID", [noteIDValidator, exists], deleteTask)
+router.patch("/:noteID", [exists, noteIDValidator], updateTask)
+
 
 module.exports = router
