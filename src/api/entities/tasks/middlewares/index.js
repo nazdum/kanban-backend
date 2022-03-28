15b43ec2babc084c71c2d1d4a8e5454cbc96691e
Nazdum <camilo.mendez@uao.edu.co>
2022-03-27 19:38:39 -0500

    indexado el middleware de verificacion de userID

--- src/api/entities/tasks/middlewares/index.js
+++ src/api/entities/tasks/middlewares/index.js
@@ -1,9 +1,11 @@
 const taskValidator = require('./taskValidator')
 const noteIDValidator = require('./noteIDValidator')
 const exists = require('./exists')
+const userIDValidator = require('./userIDValidator')
 
 module.exports = {
     taskValidator,
     noteIDValidator,
-    exists
+    exists,
+    userIDValidator
 }
