de646fd5da04b2ae74980d635a15165ccc74242f
Nazdum <camilo.mendez@uao.edu.co>
2022-03-27 20:13:06 -0500

    se cambio el orden de los middlewares

--- src/api/entities/tasks/task.routes.js
+++ src/api/entities/tasks/task.routes.js
@@ -4,8 +4,8 @@ const { createTask, deleteTask, updateTask } = require('./task.controllers');
 const { taskValidator, exists, noteIDValidator, userIDValidator } = require('./middlewares/');
 
 router.post("/", taskValidator, createTask);
-router.delete("/:noteID", [userIDValidator, noteIDValidator, exists], deleteTask)
-router.patch("/:noteID", [exists, noteIDValidator], updateTask)
+router.delete("/:noteID", [noteIDValidator, exists , userIDValidator], deleteTask)
+router.patch("/:noteID", [noteIDValidator, exists ], updateTask)
 
 
 module.exports = router
