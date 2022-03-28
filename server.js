56589d8e2171bc48548725d49dcdb534f6fa1981
Nazdum <camilo.mendez@uao.edu.co>
2022-03-28 15:35:51 -0500

    modificado para mostrar el entorno de desarrollo en el que se esta actualmente

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
  * Import node modules
 */
 const express = require('express');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv'); dotenv.config()
 const morgan = require('morgan');
 
 /**
@@ -15,7 +15,7 @@ const UserRouter = require('./src/api/entities/user/user.routes')
 /**
  * SetUp 
 */
-dotenv.config()
+const { startConnection } = require('./src/api/database/database')
 const app = express();
 app.set('port', 5000 || process.env.PORT);
 
@@ -37,9 +37,9 @@ app.use("/user", UserRouter);
 const server = app.listen(
     app.get('port'),
     () => {
-        console.log(`Server on port: ${app.get('port')}`)
-        const db = require('./src/api/database/database')
+        console.log(`Server running on port: ${app.get('port')} \n***${process.env.NODE_ENV} mode***`)
+        startConnection()
     }
 )
 
-module.exports = { server, app }
+module.exports = { app, server }
